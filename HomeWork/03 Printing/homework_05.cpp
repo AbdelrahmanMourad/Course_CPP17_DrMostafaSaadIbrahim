609eabf0397261af76366dd9353f55d41ff54d76
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-09-26 19:42:45 +0300

    03 Printing HomeWork

---
+++ HomeWork/03 Printing/homework_05.cpp
@@ -0,0 +1,40 @@
+// Homework 1: Find all errors and fix them
+
+// #include<stream
+#include <iostream>
+// using namespace STD
+using namespace std;
+
+// cout <<"work smart not hard\n";
+// Will never be executed outside of main funcion.
+
+// int maIN() {
+int main()
+{
+
+	// cout << "Children must be taught how to think, not what to think\n"
+	cout << "Children must be taught how to think, not what to think\n";
+
+	// cout << "We worry about what a child will become "tomorrow", yet we forget that he is someone today\n";
+	cout << "We worry about what a child will become \"tomorrow\", yet we forget that he is someone today\n";
+
+	// cout << "Children are not things to be molded"<", but are people to be unfolded\n";
+	cout << "Children are not things to be molded"
+		 << ", but are people to be unfolded\n";
+
+	// cout << "Each day of our lives we make deposits in the memory banks of our children."<<end;
+	cout << "Each day of our lives we make deposits in the memory banks of our children." << endl;
+
+	// cout      << ""It is easier to build strong children than to repair broken men""<<"\n";
+	cout << "\"It is easier to build strong children than to repair broken men\""
+		 << "\n";
+
+	// cout >> "Children need models rather than critics\n";
+	cout << "Children need models rather than critics\n";
+
+	// out<<"Children have never been very good at listening to their elders, but they have never failed to imitate them";
+	cout << "Children have never been very good at listening to their elders, but they have never failed to imitate them";
+
+	// cout < "Children are our most valuable resource\n";
+	cout << "Children are our most valuable resource\n";
+}
